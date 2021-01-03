b1c2b3374bac510c778dbd55ad14cf6103fc731b
Jason Antwi-Appah <hey@jasonaa.me>
2021-01-03 04:27:05 +0000

    feat: add api for getting recent gh commits, add github action to remind me to commit daily

---
+++ api/streaks/lib.js
@@ -0,0 +1,71 @@
+const {Octokit} = require("@octokit/rest")
+
+const octokit = new Octokit({
+    userAgent: "jasonaa-streaks-api"
+})
+const tz = "America/Chicago"
+const user = "jasonappah"
+
+function recentCommits(callback) {
+    octokit.activity
+        .listPublicEventsForUser({
+            username: user
+        })
+        .then((res) => {
+            // gets current date-time in my timezone
+            const today = new Date(new Date().toLocaleString("en-US", {timeZone: tz}))
+            const thisYear = today.getYear()
+            const thisDay = today.getDay()
+            const thisMonth = today.getMonth()
+            const thisWeek = getWeekNumber(today)
+
+            var committedToday = false
+            const pushes = res.data.filter((entry) => entry.type == "PushEvent")
+            var commits = {latest: {}, today: [], thisWeek: [], earlier: [], all: []}
+            for (let push of pushes) {
+                // this date is in UTC by default, so we convert it to my local timezone
+                const date = new Date(
+                    new Date(push.created_at).toLocaleString("en-US", {timeZone: tz})
+                )
+
+                commits.all = [...commits.all, ...push.payload.commits]
+
+                if (
+                    thisYear == date.getYear() &&
+                    thisDay == date.getDay() &&
+                    thisMonth == date.getMonth()
+                ) {
+                    committedToday = true
+                    commits.today = [...commits.today, ...push.payload.commits]
+                } else if (thisWeek == getWeekNumber(date)) {
+                    commits.thisWeek = [...commits.thisWeek, ...push.payload.commits]
+                } else {
+                    commits.earlier = [...commits.earlier, ...push.payload.commits]
+                }
+            }
+            commits.latest = commits.all[0]
+            if (!callback) {
+                return [committedToday, commits]
+            } else {
+                callback(committedToday, commits)
+            }
+            
+        })
+}
+
+// "borrowed" from https://stackoverflow.com/a/6117889
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()))
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7))
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1))
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7)
+    // Return array of year and week number
+    return [d.getUTCFullYear(), weekNo]
+}
+
+module.exports.recentCommits = recentCommits
--- api/streaks/twilio.js
+++ api/streaks/twilio.js
@@ -1,3 +1,36 @@
-// nothing yet
+console.log("running")
+const lib = require("./lib")
 
-module.exports = async (req, res) => {res.send("soon come.")}
+module.exports = async (req, res) => {
+    res.send(
+        "This is meant to be run on a schedule by a GitHub Action. To check out the source, take a look at https://jasonaa.me/g/api/tree/main/api/streaks/twilio.js"
+    )
+}
+
+const sid = process.env.TWILIO_ACCOUNT_SID
+const token = process.env.TWILIO_AUTH_TOKEN
+const sender = process.env.TWILIO_PHONE_NO
+const recipient = process.env.RECIPIENT_PHONE_NO
+
+// soon i'll implement keeping track of my streak using postgresql!
+const streak = 0
+
+if (sid && token && sender && recipient) {
+    lib.recentCommits((committed, commits) => {
+        const client = require("twilio")(sid, token)
+        var message = ""
+        if (committed == false) {
+            const message = `Hey! Looks like you haven't committed to GitHub today! You don't want to lose your streak of ${streak} days, do you?`
+        } else {
+            message = `Congrats on committing today! You now have a streak of ${streak} days!`
+        }
+        client.messages
+            .create({
+                body: message,
+                from: sender,
+                to: recipient
+            })
+            .then((message) => console.log(message))
+            .catch((e) => console.log(e))
+    })
+}
